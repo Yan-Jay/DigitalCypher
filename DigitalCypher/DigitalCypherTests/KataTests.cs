f15693d87584121763df5dbdfdcb92c33c42fcd2
DESKTOP-99UP9B2\YanJ <kj929409@gmail.com>
2018-08-18 22:22:55 +0800

    fix unit test error

--- DigitalCypher/DigitalCypherTests/KataTests.cs
+++ DigitalCypher/DigitalCypherTests/KataTests.cs
@@ -16,14 +16,14 @@ namespace DigitalCypher.Tests
         {
             var except = new int[] {20, 12, 18, 30, 21};
             var actural = Kata.Encode("scout", 1939);
-            Assert.AreEqual(except, actural);
+            CollectionAssert.AreEqual(except, actural);
         }
         [TestMethod()]
         public void EncodeTest2()
         {
             var except = new int[] { 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8 };
             var actural = Kata.Encode("masterpiece", 1939);
-            Assert.AreEqual(except, actural);
+            CollectionAssert.AreEqual(except, actural);
         }
     }
 }
